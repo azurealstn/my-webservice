5c85abfb1b9fce78246304585b6b7e8382856cd2
azurealstn <azurealstn@naver.com>
2020-08-29 22:30:51 +0900

    프로젝트 배포를 위한 커밋 단계

---
+++ src/main/java/com/minsu/springboot/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.minsu.springboot.domain.user;
+
+import com.minsu.springboot.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
--- src/test/java/com/minsu/springboot/web/IndexControllerTest.java
+++ src/test/java/com/minsu/springboot/web/IndexControllerTest.java
@@ -8,9 +8,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IndexControllerTest {
 
     @Autowired
@@ -20,6 +21,6 @@ public class IndexControllerTest {
     public void 메인페이지_로딩() {
         String body = this.restTemplate.getForObject("/", String.class);
 
-        assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스 Ver.2");
     }
 }

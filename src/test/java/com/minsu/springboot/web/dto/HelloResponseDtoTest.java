8a9551d369e6177a7beadbac1479d3865112baae
azurealstn <azurealstn@naver.com>
2020-08-29 15:20:50 +0900

    게시판 등록/수정/삭제/조회 커밋

---
+++ src/test/java/com/minsu/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.minsu.springboot.web.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}

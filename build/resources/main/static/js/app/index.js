9edea38af4d315a670f4869c9dc8bf7731f56d24
azurealstn <azurealstn@naver.com>
2020-09-03 14:23:17 +0900

    멘트 변경

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -27,7 +27,7 @@ var main = {
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
-            alert('글이 등록되었습니다.');
+            alert('글 등록이 완료되었습니다~');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
@@ -48,7 +48,7 @@ var main = {
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
-            alert('글이 수정되었습니다.');
+            alert('글 수정이 완료되었습니다~');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
@@ -63,7 +63,7 @@ var main = {
             dataType: 'json',
             contentType:'application/json; charset=utf-8'
         }).done(function() {
-            alert('글이 삭제되었습니다.');
+            alert('글 삭제가 완료되었어요!');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
